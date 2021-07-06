a7cedbd386b39f6ea904da5676c4ac0e7c7b171e
altegani mohammed <tiganimoh@gmail.com>
2021-07-06 17:58:19 +0200

    add type convertor

---
+++ app/src/main/java/com/example/runningtrackerapp/db/RunningDataBase.kt
@@ -0,0 +1,15 @@
+package com.example.runningtrackerapp.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+
+
+@Database(
+    entities = [Run::class],
+    version = 1
+)
+@TypeConverters(Convertors::class)
+abstract class RunningDataBase : RoomDatabase(){
+}
