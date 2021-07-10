a7d420d6a4e035fd2e25170f2dd0964100e5998f
altegani mohammed <tiganimoh@gmail.com>
2021-07-10 14:47:17 +0200

    implement stop whatch

--- app/src/main/java/com/example/runningtrackerapp/other/Constants.kt
+++ app/src/main/java/com/example/runningtrackerapp/other/Constants.kt
@@ -15,6 +15,7 @@ object Constants {
   const val POLYLINE_COLOR = Color.RED
   const val POLYLINE_WIDTH = 18f
   const val MAP_ZOOM = 15f
+  const val TIMER_UPDATE_INTERVAL = 50L
   const val NOTIFICATION_CHANNEL_ID = "tracking_channel"
   const val NOTIFICATION_CHANNEL_NAME = "Tracking"
   const val NOTIFICATION_ID = 1
