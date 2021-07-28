4090a225495878efbbad8b3b534ff4d25d187653
altegani mohammed <tiganimoh@gmail.com>
2021-07-28 16:18:25 +0200

    add total stats and settings

--- app/src/main/java/com/example/runningtrackerapp/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/runningtrackerapp/ui/fragments/SettingsFragment.kt
@@ -1,20 +1,28 @@
 package com.example.runningtrackerapp.ui.fragments
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import com.example.runningtrackerapp.R
 import com.example.runningtrackerapp.databinding.FragmentSettingsBinding
+import com.example.runningtrackerapp.other.Constants.KEY_NAME
+import com.example.runningtrackerapp.other.Constants.KEY_WEIGHT
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 
 @AndroidEntryPoint
 class SettingsFragment : Fragment() {
 
     private lateinit var fragmentSettingsBinding: FragmentSettingsBinding
+    @Inject
+    lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
         fragmentSettingsBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_settings,container,false)
@@ -22,5 +30,47 @@ class SettingsFragment : Fragment() {
         return fragmentSettingsBinding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        loadSharedPreferences()
+        fragmentSettingsBinding.btnApplyChanges.setOnClickListener {
+            val success = applyChangesToSharedPref()
+            if(success){
+                Snackbar.make(
+                   view,
+                    "Saved Changes",
+                    Snackbar.LENGTH_LONG
+                ).show()
+            }else{
+                Snackbar.make(
+                    view,
+                    "Please Fill out All Fields",
+                    Snackbar.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+    private fun loadSharedPreferences(){
+        val name = sharedPreferences.getString(KEY_NAME,"")
+        val weight = sharedPreferences.getFloat(KEY_WEIGHT,80f)
+        fragmentSettingsBinding.etName.setText(name)
+        fragmentSettingsBinding.etWeight.setText(weight.toString())
+
+    }
+    private fun applyChangesToSharedPref():Boolean{
+      val nameText = fragmentSettingsBinding.etName.text.toString()
+      val weight = fragmentSettingsBinding.etWeight.text.toString()
+      if(nameText.isEmpty() || weight.isEmpty()){
+             return false
+      }
+       sharedPreferences.edit()
+           .putString(KEY_NAME,nameText)
+           .putFloat(KEY_WEIGHT,weight.toFloat())
+           .apply()
+        val toolBar = "Let's go $nameText"
+        requireActivity().findViewById<TextView>(R.id.tvToolbarTitle).text = toolBar
+        return true
+
+    }
 
 }
--- app/src/main/java/com/example/runningtrackerapp/ui/fragments/StatisticFragment.kt
+++ app/src/main/java/com/example/runningtrackerapp/ui/fragments/StatisticFragment.kt
@@ -1,33 +1,62 @@
 package com.example.runningtrackerapp.ui.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import com.example.runningtrackerapp.R
+import com.example.runningtrackerapp.databinding.FragmentStatisticBinding
+import com.example.runningtrackerapp.other.TrackingUtility
 import com.example.runningtrackerapp.ui.viewmodels.StatisticViewModels
 import dagger.hilt.android.AndroidEntryPoint
+import kotlin.math.round
 
 @AndroidEntryPoint
 class StatisticFragment : Fragment() {
 
     private val statisticViewModels: StatisticViewModels by viewModels()
+    private lateinit var fragmentStatisticBinding: FragmentStatisticBinding
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_statistic, container, false)
+        fragmentStatisticBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_statistic,container,false)
+        fragmentStatisticBinding.lifecycleOwner = this
+        return fragmentStatisticBinding.root
     }
 
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        subscriberToObservers()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun subscriberToObservers(){
+        statisticViewModels.totalTimeRun.observe(viewLifecycleOwner,{
+            it?.let {
+                fragmentStatisticBinding.tvTotalTime.text = TrackingUtility.getFormattedStopWatchTime(it)
+            }
+        })
+        statisticViewModels.totalDistance.observe(viewLifecycleOwner,{
+            it?.let {
+                val km = it / 1000f
+                "${round(km * 10f) / 10f}km".also { fragmentStatisticBinding.tvTotalDistance.text = it }
+            }
+        })
+        statisticViewModels.totalAvgSpeed.observe(viewLifecycleOwner,{
+            it?.let {
+                "${round(it * 10f) / 10f}km/h".also { fragmentStatisticBinding.tvAverageSpeed.text = it }
+            }
+        })
+        statisticViewModels.totalCalories.observe(viewLifecycleOwner,{
+            it?.let {
+                fragmentStatisticBinding.tvTotalCalories.text = "${it}kcal"
+            }
+        })
+    }
 }
